6a09a1046ebfad442ad1e29f5ce164a05c52f9f6
Alessandra Versari <alessandra.versari2@studio.unibo.it>
2025-06-20 18:46:29 +0200

    Added message protocols

--- distributed/src/main/scala/it/unibo/agar/Message.scala
+++ distributed/src/main/scala/it/unibo/agar/Message.scala
@@ -1,4 +1,21 @@
 package it.unibo.agar
 
+import it.unibo.agar.model.{Food, Player}
+
 /** Tag interface for all messages sends by actors */
 trait Message
+
+object WorldProtocol:
+  case class UpdatePlayerMovement(playerId: String, x: Double, y:Double) extends Message
+  case class RemoveFood(foodId: String) extends Message
+  case class RemovePlayer(playerId: String) extends Message
+  case class UpdatePlayerScore(playerId: String, newScore: Double) extends Message
+  case class NotifyVictory(playerId: String, score: Double) extends Message
+
+object PlayerProtocol:
+  case class Move(x: Double, y: Double) extends Message
+  case object Tick extends Message
+  case class FoodCollision(food: Food) extends Message
+  case class PlayerCollision(player: Player) extends Message
+  case class CurrentScore(score: Double) extends Message
+
